762bb082f92000dd0e8f29102bca13e0db1f04e2
Dima Bogdanov <homelion432@gmail.com>
2021-09-27 23:31:59 +0300

    add binding and fix password validation

--- app/src/main/java/com/bogdanov/strava/Login.kt
+++ app/src/main/java/com/bogdanov/strava/Login.kt
@@ -11,64 +11,49 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import androidx.core.view.get
+import androidx.core.widget.doAfterTextChanged
 import androidx.core.widget.doOnTextChanged
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModel
 import androidx.navigation.fragment.findNavController
+import by.kirich1409.viewbindingdelegate.viewBinding
+import com.bogdanov.strava.databinding.FragmentLoginBinding
 import com.google.android.material.textfield.TextInputEditText
 import com.google.android.material.textfield.TextInputLayout
 import timber.log.Timber
 
-
 class LoginPage : Fragment(R.layout.fragment_login) {
 
     private val viewModel : LoginViewModel by viewModels()
+    private val binding : FragmentLoginBinding by viewBinding()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val loginButton = view.findViewById<Button>(R.id.login)
+        binding.email.editText?.setText(viewModel.state.value?.email)
+        binding.password.editText?.setText(viewModel.state.value?.password)
 
-        val emailTextView = view.findViewById<TextInputLayout>(R.id.email)
-
-        val passwordTextView = view.findViewById<TextInputLayout>(R.id.password)
-
-
-        passwordTextView.editText?.doOnTextChanged{ passwordText, _, _, _ ->
-            viewModel.updateStatus(emailTextView.editText?.text.toString(),passwordText.toString())
-
-            if (passwordText!!.length < 8)
-                passwordTextView.error = getString(R.string.passwordTextView_error)
-            else
-                passwordTextView.error = null
-
+        binding.email.editText?.doOnTextChanged { emailText, _, _, _ ->
+            viewModel.updateStatus(emailText.toString(),binding.password.editText?.text.toString())
         }
 
-
-        emailTextView.editText?.doOnTextChanged { emailText, _, _, _ ->
-            viewModel.updateStatus(emailText.toString(),passwordTextView.editText?.text.toString())
-
+        binding.password.editText?.doOnTextChanged { passwordText, _, _, _ ->
+            viewModel.updateStatus(binding.email.editText?.text.toString(), passwordText.toString())
+            viewModel.setError(passwordText.toString(), getString(R.string.passwordTextView_error))
         }
 
         viewModel.state.observe(viewLifecycleOwner, { state ->
-
-            if (emailTextView.editText?.text.toString()!=state.email){
-                emailTextView.editText?.setText(state.email)
-            }
-
-            if (passwordTextView.editText?.text.toString()!=state.password){
-                passwordTextView.editText?.setText(state.password)
-            }
-
-            loginButton.isEnabled = state.buttonEnableStatus
-
+            binding.login.isEnabled = state.buttonEnableStatus
         })
 
-        loginButton.setOnClickListener {
+        viewModel.errorMessage.observe(viewLifecycleOwner, {errorMessage ->
+            binding.password.error = errorMessage
+        })
+
+        binding.login.setOnClickListener {
             findNavController().navigate(R.id.action_loginPage_to_mainPage)
         }
     }
-
 }
--- app/src/main/java/com/bogdanov/strava/LoginViewModel.kt
+++ app/src/main/java/com/bogdanov/strava/LoginViewModel.kt
@@ -1,16 +1,15 @@
 package com.bogdanov.strava
 
 import android.util.Patterns
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.SavedStateHandle
-import androidx.lifecycle.ViewModel
+import androidx.lifecycle.*
 
 class LoginViewModel(savedStateHandle: SavedStateHandle) : ViewModel() {
 
     private val mutableState = savedStateHandle.getLiveData("loginStatus", LoginParcelable(email = "", password = "", false))
-
     val state : LiveData<LoginParcelable> = mutableState
+
+    private val _errorMessage = MutableLiveData<String?>()
+    val errorMessage : LiveData<String?> = _errorMessage
 
     fun updateStatus(email: String, password: String) {
         val buttonEnableStatus =
@@ -18,9 +17,16 @@ class LoginViewModel(savedStateHandle: SavedStateHandle) : ViewModel() {
         mutableState.value = LoginParcelable(email, password, buttonEnableStatus)
     }
 
+    fun setError(password: String, errorText: String){
+        if (validPassword(password)){
+            _errorMessage.value = null
+        }
+        else{
+            _errorMessage.value = errorText
+        }
+    }
 
     private fun validPassword(password: String): Boolean = password.length >= 8 && password.isNotEmpty()
 
     private fun validEmail(email:String): Boolean = Patterns.EMAIL_ADDRESS.matcher(email).matches() && email.isNotEmpty()
-
 }
