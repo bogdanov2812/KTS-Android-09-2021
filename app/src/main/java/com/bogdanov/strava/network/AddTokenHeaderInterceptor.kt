3ecd1cdd4dd0ed3150fd65d3b1d40a98f18914af
Dima Bogdanov <homelion432@gmail.com>
2021-10-18 22:39:41 +0300

    Token from key-value store

--- app/src/main/java/com/bogdanov/strava/network/AddTokenHeaderInterceptor.kt
+++ app/src/main/java/com/bogdanov/strava/network/AddTokenHeaderInterceptor.kt
@@ -1,15 +1,24 @@
 package com.bogdanov.strava.network
 
+import android.app.Application
+import android.content.Context
+import android.content.SharedPreferences
+import androidx.datastore.dataStore
+import com.bogdanov.strava.MainActivity
+import com.bogdanov.strava.StravaApplication
 import com.bogdanov.strava.auth.AuthConfig
 import okhttp3.Interceptor
 import okhttp3.Response
 
-class AddTokenHeaderInterceptor : Interceptor {
+class AddTokenHeaderInterceptor(context: Context) : Interceptor {
+
+    private val sharedPreferences = context.getSharedPreferences("Auth", Context.MODE_PRIVATE)
+    private val token = sharedPreferences.getString("token", null)
+
     override fun intercept(chain: Interceptor.Chain): Response {
-        val token = AuthConfig.token
-        println(token)
         val originalRequest = chain.request()
 
+        println(token)
         val request = if (token != null) {
             originalRequest.newBuilder()
                 .header("Authorization", "Bearer $token")
