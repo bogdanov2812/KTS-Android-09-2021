a498bc28bbde8e8bf00e49834c14c05d87324ea4
Dima Bogdanov <homelion432@gmail.com>
2021-10-18 22:41:22 +0300

    Show onboarding only one time

--- app/src/main/java/com/bogdanov/strava/onboarding/ViewPagerFragment.kt
+++ app/src/main/java/com/bogdanov/strava/onboarding/ViewPagerFragment.kt
@@ -1,8 +1,10 @@
 package com.bogdanov.strava.onboarding
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.View
+import androidx.navigation.fragment.findNavController
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.bogdanov.strava.R
 import com.bogdanov.strava.databinding.FragmentViewPagerBinding
@@ -15,6 +17,12 @@ class ViewPagerFragment : Fragment(R.layout.fragment_view_pager) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        val sharedPreferences = requireContext().getSharedPreferences("onBoarding", Context.MODE_PRIVATE)
+
+        if(sharedPreferences.getBoolean("Finished", false)) {
+            findNavController().navigate(R.id.action_viewPagerFragment_to_authFragment)
+        }
 
         val fragmentList = arrayListOf<Fragment>(
             OnBoardingFirstFragment(),
--- app/src/main/java/com/bogdanov/strava/onboarding/screens/OnBoardingThirdFragment.kt
+++ app/src/main/java/com/bogdanov/strava/onboarding/screens/OnBoardingThirdFragment.kt
@@ -1,18 +1,14 @@
 package com.bogdanov.strava.onboarding.screens
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
-import androidx.viewpager2.widget.ViewPager2
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.bogdanov.strava.R
-import com.bogdanov.strava.databinding.FragmentOnBoardingFirstBinding
 import com.bogdanov.strava.databinding.FragmentOnBoardingThirdBinding
+import com.bogdanov.strava.onboarding.OnBoardingFragment
 
-class OnBoardingThirdFragment : Fragment(R.layout.fragment_on_boarding_third){
+class OnBoardingThirdFragment : OnBoardingFragment(R.layout.fragment_on_boarding_third){
     private val binding: FragmentOnBoardingThirdBinding by viewBinding()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -20,7 +16,7 @@ class OnBoardingThirdFragment : Fragment(R.layout.fragment_on_boarding_third){
 
         binding.buttonFinish.setOnClickListener {
             findNavController().navigate(R.id.action_viewPagerFragment_to_authFragment)
+            onBoardingFinish()
         }
-
     }
 }
